17bc5882b3d327c942b533e72867d11bea42ba98
lihua <3050539989@qq.com>
2024-12-28 19:13:35 +0800

    1

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,7 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 // 导入views中的页面组件
 import Login from '../views/Login.vue';  // 登录页面
-
+import menu from '../views/menu.vue'
 // 创建路由实例
 const router = createRouter({
   history: createWebHistory(),  // 使用 HTML5 历史模式
@@ -16,8 +16,13 @@ const router = createRouter({
       path: '/login',  // 显示登录页面
       name: 'Login',
       component: Login,  // 显示登录页面
-    }
+    },
     // 你可以根据需求在这里添加更多的路由
+    {
+      path: '/',  // 根路径，显示 Login 组件
+      name: 'menu',
+      component:menu ,  // 默认显示登录页面
+    }
   ]
 });
 
