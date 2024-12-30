3aadeafc3ca9e143b29c25dcd1f952f95f1a3b52
Qinghui Zuo <1415806690@qq.com>
2024-12-30 23:21:27 +0800

    comment the structs

--- internal/models/model.go
+++ internal/models/model.go
@@ -53,140 +53,169 @@ type File struct {
 	Suffix string `gorm:"column:suffix; not null" json:"Suffix"`
 	FileId uint   `gorm:"column:fileId; primaryKey" form:"FileId" binding:"required"`
 }
+
+// 客商（户）类型
+type MercType struct {
+	MercType   string `gorm:"column:mercType; not null; unique" json:"MercType"`
+	MercTypeId uint   `gorm:"column:mercTypeId; primaryKey; " form:"MercTypeId"`
+}
+
+// 供应商类型
 type SuprType struct {
 	SuprType   string `gorm:"column:suprType; not null; unique" json:"SuprType"`
 	SuprTypeId uint   `gorm:"column:suprTypeId; primaryKey; " form:"SuprTypeId"`
 }
 
+// 产品类型
 type PrdtType struct {
 	PrdtType   string `gorm:"column:prdtType; not null; unique" json:"PrdtType"`
 	PrdtTypeId uint   `gorm:"column:prdtTypeId; primaryKey; " form:"PrdtTypeId"`
 }
 
+// 食品添加剂类型
 type FoodAddType struct {
 	FoodAddType   string `gorm:"column:foodAddType; not null; unique" json:"FoodAddType"`
 	FoodAddTypeId uint   `gorm:"column:foodAddTypeId; primaryKey; " form:"FoodAddTypeId"`
 }
 
+// 饲料添加剂类型
 type FeedAddType struct {
 	FeedAddType   string `gorm:"column:feedAddType; not null; unique" json:"FeedAddType"`
 	FeedAddTypeId uint   `gorm:"column:feedAddTypeId; primaryKey; " form:"FeedAddTypeId"`
 }
 
+// 计量单位
 type UnitMeas struct {
 	UnitMeas   string `gorm:"column:unitMeas; not null; unique" json:"UnitMeas"`
 	UnitMeasId uint   `gorm:"column:unitMeasId; primaryKey; " form:"UnitMeasId"`
 }
 
+// 包装类型
 type PackType struct {
 	PackType   string `gorm:"column:packType; not null; unique" json:"PackType"`
 	PackTypeId uint   `gorm:"column:packTypeId; primaryKey; " form:"PackTypeId"`
 }
 
+// 集装箱类型
 type ConType struct {
 	ConType   string `gorm:"column:conType; not null; unique" json:"ConType"`
 	ConTypeId uint   `gorm:"column:conTypeId; primaryKey; " form:"ConTypeId"`
 }
 
+// 币种
 type Currency struct {
 	Currency   string `gorm:"column:currency; not null; unique" json:"Currency"`
 	CurrencyId uint   `gorm:"column:currencyId; primaryKey; " form:"CurrencyId"`
 }
 
+// 贸易条款
 type TradeTerm struct {
 	TradeTerm   string `gorm:"column:tradeTerm; not null; unique" json:"TradeTerm"`
 	TradeTermId uint   `gorm:"column:tradeTermId; primaryKey; " form:"TradeTermId"`
 }
 
+// 国家
 type Nation struct {
 	Nation   string `gorm:"column:nation; not null; unique" json:"Nation"`
 	NationId uint   `gorm:"column:nationId; primaryKey; " form:"NationId"`
 }
 
+// 港口
 type Port struct {
 	Port   string `gorm:"column:port; not null; unique" json:"Port"`
 	PortId uint   `gorm:"column:portId; primaryKey; " form:"PortId"`
 }
 
+// 税号类型
 type TaxType struct {
 	TaxType   string `gorm:"column:taxType; not null; unique" json:"TaxType"`
 	TaxTypeId uint   `gorm:"column:taxTypeId; primaryKey; " form:"TaxTypeId"`
 }
 
+// 品牌类型
 type BrandType struct {
 	BrandType   string `gorm:"column:brandType; not null; unique" json:"BrandType"`
 	BrandTypeId uint   `gorm:"column:brandTypeId; primaryKey; " form:"BrandTypeId"`
 }
 
+// 学历
 type Degree struct {
 	Degree   string `gorm:"column:degree; not null; unique" json:"Degree"`
 	DegreeId uint   `gorm:"column:degreeId; primaryKey; " form:"DegreeId"`
 }
 
+// 公司部门
 type Dept struct {
 	Dept   string `gorm:"column:dept; not null; unique" json:"Dept"`
 	DeptId uint   `gorm:"column:deptId; primaryKey; " form:"DeptId"`
 }
 
+// 公司岗位
 type Post struct {
 	Post   string `gorm:"column:post; not null; unique" json:"Post"`
 	PostId uint   `gorm:"column:postId; primaryKey; " form:"PostId"`
 }
 
+// 质量标准
 type QualStd struct {
 	QualStd   string `gorm:"column:qualStd; not null; unique" json:"QualStd"`
 	QualStdId uint   `gorm:"column:qualStdId; primaryKey; " form:"QualStdId"`
 }
 
+// 库存地点位置
 type InvLoc struct {
 	InvLoc   string `gorm:"column:invLoc; not null; unique" json:"InvLoc"`
 	InvLocId uint   `gorm:"column:invLocId; primaryKey; " form:"InvLocId"`
 }
 
+// 单据要求
 type DocReq struct {
 	DocReq   string `gorm:"column:docReq; not null; unique" json:"DocReq"`
 	DocReqId uint   `gorm:"column:docReqId; primaryKey; " form:"DocReqId"`
 }
 
+// 转账方式
 type PayMth struct {
 	PayMth   string `gorm:"column:payMth; not null; unique" json:"PayMth"`
 	PayMthId uint   `gorm:"column:payMthId; primaryKey; " form:"PayMthId"`
 }
 
+// 后付款转账期限
 type PayLimit struct {
 	PayLimit   string `gorm:"column:payLimit; not null; unique" json:"PayLimit"`
 	PayLimitId uint   `gorm:"column:payLimitId; primaryKey; " form:"PayLimitId"`
 }
 
+// 财务单据状态
 type FinaDocStatus struct {
 	FinaDocStatus   string `gorm:"column:finaDocStatus; not null; unique" json:"FinaDocStatus"`
 	FinaDocStatusId uint   `gorm:"column:finaDocStatusId; primaryKey; " form:"FinaDocStatusId"`
 }
 
+// 财务单据类型
 type FinaDocType struct {
 	FinaDocType   string `gorm:"column:finaDocType; not null; unique" json:"FinaDocType"`
 	FinaDocTypeId uint   `gorm:"column:finaDocTypeId; primaryKey; " form:"FinaDocTypeId"`
 }
 
+// 费用类型
 type ExpType struct {
 	ExpType   string `gorm:"column:expType; not null; unique" json:"ExpType"`
 	ExpTypeId uint   `gorm:"column:expTypeId; primaryKey; " form:"ExpTypeId"`
 }
 
+// 收费标准
 type Rates struct {
 	Rates   string `gorm:"column:rates; not null; unique" json:"Rates"`
 	RatesId uint   `gorm:"column:ratesId; primaryKey; " form:"RatesId"`
 }
 
+// 业务单据状态
 type BussOrderSta struct {
 	BussOrderSta   string `gorm:"column:bussOrderSta; not null; unique" json:"BussOrderSta"`
 	BussOrderStaId uint   `gorm:"column:bussOrderStaId; primaryKey; " form:"BussOrderStaId"`
 }
 
-type MercType struct {
-	MercType   string `gorm:"column:mercType; not null; unique" json:"MercType"`
-	MercTypeId uint   `gorm:"column:mercTypeId; primaryKey; " form:"MercTypeId"`
-}
 type Message struct {
 	Acct          []Acct
 	AcctBank      []AcctBank
